c077e87c1a55cbbd713071a07ea07d7499eee3cc
David Rolle <miridius@miridi.us>
2021-02-14 19:32:00 +0100

    Move index.html contents into App.svelte

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,30 +1,109 @@
 <script lang="ts">
-	export let name: string;
+  let snpList = `6 47916081 CG
+6 48072811 CA MainSNP
+8 12485673 AT OtherSNP`;
+
+  let seqList =
+    'Abcc6a GCTGGTATGCCTAGCAAGAGCTCTTCTCAGAAAGACAAAAGTTCTGGTGC[TG]GGATGAGGCAACAGCTGCACTGGACCTGGAGACAGACACGCTGATCCAGT';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head><title>K designer</title></svelte:head>
+
+<div class="block heading">
+  <p>K primer designer</p>
+  <div style="font-size: small">
+    &copy; 2013-2018 Eirinn Mackay, Hubrecht Institute & University College
+    London
+  </div>
+  <p style="font-size: small; font-weight: bold;">
+    Updated 2018: now using danRer11
+  </p>
+</div>
+<div class="block main">
+  <p>
+    Option 1: Provide a list of SNPs with chromosome, position, reference base
+    and mutant base.
+  </p>
+  <p>
+    A name can be provided (optional). The appropriate sequence will be taken
+    from danRer11.
+  </p>
+  <form name="form1" method="post" action="genome">
+    <p>
+      <textarea name="list" bind:value={snpList} cols="72" rows="12" />
+    </p>
+    <input
+      type="submit"
+      name="submit"
+      id="submit"
+      value="Generate primers from genome"
+    />
+  </form>
+</div>
+<div class="block main">
+  <p>
+    Option 2: Provide a sequence with the mutation in the form [WM], where W is
+    wildtype base and M is mutant.
+  </p>
+  <p>Include 50bp on either side of the mutation.</p>
+  <p>
+    For deletions, use the shortest common sequence and make M the first base
+    after the deletion.
+  </p>
+  <form name="form2" method="post" action="sequence">
+    <p>
+      <textarea
+        name="list"
+        bind:value={seqList}
+        cols="120"
+        rows="12"
+        style="font-size: 8pt"
+      />
+    </p>
+    <input
+      type="submit"
+      name="submit"
+      id="submitseq"
+      value="Generate primers from sequence"
+    />
+  </form>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  :global(body) {
+    font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
+    font-size: 12pt;
+    background-image: url(body-bg.jpg);
+    background-repeat: repeat;
+  }
+  .block {
+    width: 850px;
+    margin: 4pt;
+    margin-left: 20px;
+    padding: 5px;
+    border-top-width: thin;
+    border-right-width: thin;
+    border-bottom-width: thin;
+    border-left-width: thin;
+    border-top-style: solid;
+    border-right-style: solid;
+    border-bottom-style: solid;
+    border-left-style: solid;
+  }
+  .block.heading {
+    font-size: 36pt;
+    background-color: #bbcfff;
+    margin-top: 30px;
+  }
+  p {
+    margin: 0;
+  }
+  .block.main {
+    background-color: #ccecff;
+    margin-top: 8pt;
+  }
+  textarea {
+    font-family: 'Courier New', Courier, monospace;
+    width: 840px;
+  }
 </style>
